db34432929041b0051f90b5021dc0807fe71258d
unknown <cristian.daniel.herrera@hotmail.com>
2022-01-08 00:00:44 +0000

    make it better

--- models/server.js
+++ models/server.js
@@ -21,16 +21,11 @@ class Server {
   }
 
   middlewares() {
-    var whitelist = ["http://localhost:3000"];
-    var corsOptions = {
-      origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-    };
+     
+ var corsOptions = {
+  origin: ['http://localhost:3000'],
+   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
     // Cors
     this.app.use(cors(corsOptions));
