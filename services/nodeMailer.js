c6ee17ceb86be18712f9162e4ae6bf8c595da80e
unknown <cristian.daniel.herrera@hotmail.com>
2022-01-08 21:19:40 +0000

    ok

--- services/nodeMailer.js
+++ services/nodeMailer.js
@@ -8,6 +8,7 @@ const nodemailer = require("nodemailer");
 function userSendMessage(subject, email, text) {
   const transporter = nodemailer.createTransport({
     service: "hotmail",
+    port: 465,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
